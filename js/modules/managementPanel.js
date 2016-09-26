b40e56b95c2829754967fbd6e928e554f5483273
floLfg <florian.laforgue@gmail.com>
2016-09-26 17:41:31 +0200

    Petite réorganisation de managementPanel.js

--- js/modules/managementPanel.js
+++ js/modules/managementPanel.js
@@ -13,58 +13,65 @@ ManagementPanel.startAction = function(action) {
 		}
 	}
 };
+
 ManagementPanel.getNbAntsLabelForAction = function(action) {
 	return $('#nb-' + action + '-ants');
 };
+
 ManagementPanel.getNbAntsLabelsForAllActions = function() {
 	return $('.action-nb-ants-label');
 };
-ManagementPanel.addAntActionButtonHandler = function(action) {
-	ManagementPanel.addAntAction(action);
+
+ManagementPanel.addAntActionButtonHandler = function(action) {ManagementPanel.addOrRemoveAntActionButtonHandler(action, true);};
+ManagementPanel.removeAntActionButtonHandler = function(action) {ManagementPanel.addOrRemoveAntActionButtonHandler(action, false);};
+ManagementPanel.addOrRemoveAntActionButtonHandler = function(action, actionIsAdd) {
+	ManagementPanel.addOrRemoveAction(action, actionIsAdd);
 	ManagementPanel.changeAntActionInterval = setInterval(function() {
+		ManagementPanel.addOrRemoveAction(action, actionIsAdd);
+	}, 200, actionIsAdd);
+};
+ManagementPanel.addOrRemoveAction = function(action, actionIsAdd) {
+	if (actionIsAdd) {
 		ManagementPanel.addAntAction(action);
-	}, 200);
+	} else {
+		ManagementPanel.removeAntAction(action);
+	}
 };
 ManagementPanel.addAntAction = function(action) {
 	if (game.anthill.hasWaitingAnt()) {
 		var ant = game.anthill.getWaitingAnt();
     	ant.startAction(action);
 		
-		ManagementPanel.getNbAntsLabelForAction('waiting').text(game.anthill.activities_done['waiting']);
-    	ManagementPanel.getNbAntsLabelForAction(action).text(game.anthill.activities_done[action]);
+		ManagementPanel.updateNbAntsForAction(action);
+    	ManagementPanel.updateNbAntsForAction('waiting');
 	}
-};
-ManagementPanel.removeAntActionButtonHandler = function(action) {
-	ManagementPanel.removeAntAction(action);
-	ManagementPanel.changeAntActionInterval = setInterval(function() {
-		ManagementPanel.removeAntAction(action);
-	}, 200);
 };
 ManagementPanel.removeAntAction = function(action) {
 	if (game.anthill.hasAntOfActivity(action)) {
 		var ant = game.anthill.getAntOfActivity(action);
     	ant.startWaiting();
 
-    	ManagementPanel.getNbAntsLabelForAction('waiting').text(game.anthill.activities_done['waiting']);
-    	ManagementPanel.getNbAntsLabelForAction(action).text(game.anthill.activities_done[action]);
+    	ManagementPanel.updateNbAntsForAction(action);
+    	ManagementPanel.updateNbAntsForAction('waiting');
 	}
 };
+
 ManagementPanel.clearChangeAntActionInterval = function() {
 	clearInterval(ManagementPanel.changeAntActionInterval);
 };
-ManagementPanel.updateNbAntsBarAndLabel = function() {
-	var antsLevel = (game.anthill.ants.length / game.anthill.nb_ants_max) * 100;
-	document.getElementById('nb-ants-bar').style.width = antsLevel + '%';
-	document.getElementById('nb-ants-label').innerHTML = game.anthill.ants.length + ' / ' + game.anthill.nb_ants_max;
-};
-ManagementPanel.updateFoodBarAndLabel = function() {
-	var foodLevel = (game.anthill.food_stock / game.anthill.max_food_stock) * 100;
-	document.getElementById('food-stock-bar').style.width = foodLevel + '%';
-	document.getElementById('food-stock-label').innerHTML = game.anthill.food_stock + ' / ' + game.anthill.max_food_stock;
-};
+
 ManagementPanel.getDefaultAntAction = function() {
 	return document.getElementById('default-action').value;
 };
+
+ManagementPanel.updateNbAntsBarAndLabel = function() {ManagementPanel.updateResourceBarAndLabel('ants', game.anthill.ants.length, game.anthill.nb_ants_max);};
+ManagementPanel.updateFoodBarAndLabel = function() {ManagementPanel.updateResourceBarAndLabel('food', game.anthill.food_stock, game.anthill.max_food_stock);};
+ManagementPanel.updateResourceBarAndLabel = function(resourceName, resourceAmount, maxResourceAmount) {
+	var percentage = (resourceAmount / maxResourceAmount) * 100;
+	document.getElementById(resourceName + '-bar').style.width = percentage + '%';
+	document.getElementById(resourceName + '-label').innerHTML = resourceAmount + ' / ' + maxResourceAmount;
+};
+
 ManagementPanel.updateNbAntsForAction = function(action) {
 	document.getElementById('nb-' + action + '-ants').innerHTML = game.anthill.activities_done[action];
 };
